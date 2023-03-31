1dc37ebb24b99b57aa3e5e1bde5759e90dde2cb9
lfelipetorquato <128148621+lfelipetorquato@users.noreply.github.com>
2023-03-31 11:33:50 -0300

    Add files via upload

---
+++ week5_homework (1).py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and System
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. dyplr
+# 2. Stingr
+# 3. ggplot
+
+# |Data Science Tools|
+# |------------------|
+# |R Studio|
+# |Apache Stark|
+# |Tensor Flow|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+
+# In[13]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[14]:
+
+
+t=200
+thour=t/60
+print(thour)
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# 
+# **objectives**
+# 
+# - list popular lenguages for Data Science
+# - Introduction to Python
+# - Introduction to R
+# 
+# 
+
+# ## Author: Luis Felipe
+
+# In[ ]:
+
+
+
+
